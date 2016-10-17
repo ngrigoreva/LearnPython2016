3aaee17213758ab652de618abf9f744c46a72ff3
ngrigoreva <nadezhda_grigoreva_97@mail.ru>
2016-10-17 19:00:29 +0300

    Update регвыры.py

--- регвыры.py
+++ регвыры.py
@@ -2,23 +2,27 @@ html = open ('хытымыэль на проверку.txt', 'r', encoding = 'ut
 
 import re
 
-#author <p style="text-align: right;" align="right">
-regAuthor = re.compile('<p style="text-align: right;"( align="right")?>.*?([ёА-Яа-я. ]+)</strong>', flags=re.U | re.DOTALL)
-author = regAuthor.search(html)
-if author:
-    author_ = author.group(2)
-    author = re.sub('\.$', '', author_)#strip тут тоже не работает
-    print(author)
-else:
-    regauthor = re.compile('<p align="right">.*?([ёА-Яа-я. ]+)</strong>', flags=re.U | re.DOTALL)
-    author = regauthor.search(html)
-    author = re.sub(r'\.$', '', author.group(1))
-    print(author)
+def get_year(html):
+    #author <p style="text-align: right;" align="right">
+    regAuthor = re.compile('<p style="text-align: right;"( align="right")?>.*?([ёА-Яа-я. ]+)</strong>', flags=re.U | re.DOTALL)
+    author = regAuthor.search(html)
+    if author:
+        author_ = author.group(2)
+        author = re.sub('\.$', '', author_)
+        #author = author_.strip('.') - strip тоже не работает
+        print(author)
+    else:
+        regauthor = re.compile('<p align="right">.*?([ёА-Яа-я. ]+)</strong>', flags=re.U | re.DOTALL)
+        author = regauthor.search(html)
+        author = re.sub(r'\.$', '', author.group(1))
+        print(author)
 
-#header - это то, что на синем фоне. в некоторых статьях (e.g., 322) есть ещё подзаголовок, его не учитываю
-regHeader = re.compile('<title>(.+) / газета За урожай, Смоленск, п. Шумячи</title>', flags=re.U | re.DOTALL)
-header = regHeader.search(html).group(1)
-print(header)
+
+def get_header(html):
+    #header - это то, что на синем фоне. в некоторых статьях (e.g., 322) есть ещё подзаголовок, его не учитываю
+    regHeader = re.compile('<title>(.+) / газета За урожай, Смоленск, п. Шумячи</title>', flags=re.U | re.DOTALL)
+    header = regHeader.search(html).group(1)
+    #print(header)
 
 #created
 
