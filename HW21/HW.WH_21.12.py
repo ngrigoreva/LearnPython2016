e64b35ba91c6cde260fecb6f00dc58064203d6b9
ngrigoreva <nadezhda_grigoreva_97@mail.ru>
2016-12-21 23:34:45 +0300

    Update HW.WH_21.12.py

--- HW21/HW.WH_21.12.py
+++ HW21/HW.WH_21.12.py
@@ -2,7 +2,8 @@ import os
 import re
 
 def mystem():
-    os.system(r'C:\\Users\\ADMIN\\mystem.exe -ncid' + 'input.txt' + ' ' + 'output.txt')
+    #os.system(r'C:\\Users\\ADMIN\\mystem.exe -ncid' + 'input.txt' + ' ' + 'output.txt')
+    os.system('C:\\Users\\ADMIN\\mystem.exe input.txt output.txt')
 
 def Tables():
     f = open('input.txt', 'r', encoding='utf-8')
@@ -26,21 +27,21 @@ def Tables():
             #wordform = dict.keys(uniqueDict)
             #lemma = dict.get(key[, default]) не работает
         sql1 = 'INSERT INTO table1 (id, wordform, lemma) VALUES (0, %s, %s)' % (wordform, uniqueDict[wordform])
-        sqlarr1.append(sql2)
+        sqlarr1.append(sql1)
     table1 = 'CREATE TABLE table1 (id INTEGER PRIMARY KEY, wordform VARCHAR, lemma VARCHAR' + str(sqlarr1)
     sqlarr2 = []
     for word in words:
             lmark = 0
             rmark = 0
             strippedWord = word.strip(' .,?!^:;\"—()[]{}')
-            #if word != strippedWord and strippedWord:
-            if word[0] != strippedWord[0]:
-                lmark = 1
-            if word[-1] != strippedWord[-1]:
-                rmark = 1                
-                i += 1
-                sql2 = 'INSERT INTO table1 (id, wordform, lmark, rmark, position, id2) VALUES (0, %s, %d, %d, %d, )' % (strippedWord, lmark, rmark, i)
-                sqlarr2.append(sql2)
+            if word != strippedWord and strippedWord:
+                if word[0] != strippedWord[0]:
+                    lmark = 1
+                if word[-1] != strippedWord[-1]:
+                    rmark = 1                
+                    i += 1
+                    sql2 = 'INSERT INTO table1 (id, wordform, lmark, rmark, position, id2) VALUES (0, %s, %d, %d, %d, )' % (strippedWord, lmark, rmark, i)
+                    sqlarr2.append(sql2)
             selectLine2 = 'SELECT table1.id from table1 WHERE table2.id = table1.id'
             table2 = 'CREATE TABLE table2 (id INTEGER PRIMARY KEY, wordform VARCHAR, lmakr INTEGER PRIMARY KEY, rmark INTEGER PRIMARY KEY, position INTEGER PRIMARY KEY, id2 INTEGER PRIMARY KEY' + '\n' + str(sqlarr2) + '\n' + selectLine2
     f1.write(table2 + '\n' + table1)
